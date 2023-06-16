a267de04b04978296ac70ce9dbaecb312625afcc
zwz <1576274385@qq.com>
2023-06-16 18:44:56 +0800

    上线

--- src/views/test/index_1.vue
+++ src/views/test/index_1.vue
@@ -9,6 +9,8 @@ import Stats from "three/examples/jsm/libs/stats.module";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { ConvexGeometry } from "three/examples/jsm/geometries/ConvexGeometry";
 import { GUI } from "three/examples/jsm/libs/dat.gui.module";
+// import dat from 'dat.gui'
+// let GUI = dat.GUI
 import { TWEEN } from "three/examples/jsm/libs/tween.module.min.js";
 var gui, scene, camera, renderer, controls, lights, mesh, bones, skeletonHelper;
 export default {
@@ -216,7 +218,7 @@ export default {
       scene.add(mesh);
 
       this.render();
-      document.getElementById("loading").style.display = "none";
+      // document.getElementById("loading").style.display = "none";
     },
     setupDatGui() {
       var bones = mesh.skeleton.bones;
